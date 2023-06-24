aa6c5249605de7bf879e5c4fa306b583ae6760b9
Kilian Koeltzsch <me@kilian.io>
2023-06-24 16:39:26 +0200

    a bit of legal formatting

--- src/legal-notice.tsx
+++ src/legal-notice.tsx
@@ -5,7 +5,9 @@ export default function LegalNotice() {
       <p>Verantwortlich für den Inhalt dieser Seite ist</p>
       <br />
       <p><b>Kilian Költzsch</b></p>
-      <p>Zwickauer Str. 91, 01187 Dresden</p>
+      <p>Zwickauer Str. 91</p>
+      <p>01187 Dresden</p>
+      <br />
       <p><a href="mailto:akronymisierbar@fastmail.com">akronymisierbar@fastmail.com</a></p>
     </>
   );
