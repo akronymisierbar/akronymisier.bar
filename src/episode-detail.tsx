404b975d2a92a1b7d9ee0cadd0f221e588993df7
Kilian Koeltzsch <me@kilian.io>
2023-06-25 00:16:47 +0200

    add back button to episode details
    also add shownotes title

--- src/episode-detail.tsx
+++ src/episode-detail.tsx
@@ -1,16 +1,18 @@
 import { useLoaderData } from "react-router-dom";
 import { getEpisodeDetails, EpisodeDetails } from "./feedparser";
-import AudioPlayer from 'react-h5-audio-player';
-import 'react-h5-audio-player/lib/styles.css';
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
 import Footer from "./footer";
 import { useEffect } from "react";
 import unshortifyEmoji from "./emoji";
+import { Link } from "react-router-dom";
+import { ReactComponent as BackIcon } from "./back-icon.svg";
 
 interface LoaderData {
   episode: EpisodeDetails;
 }
 
-export async function episodeLoader({ params }: {params: any}) {
+export async function episodeLoader({ params }: { params: any }) {
   const episode = await getEpisodeDetails(params.episode);
   return { episode };
 }
@@ -24,19 +26,34 @@ export default function EpisodeDetail() {
 
   return (
     <>
-      <h1>{episode.title}</h1>
-      <p className='details'>
-        <span className="date">{episode.date}</span>
-        {' '} · {' '}
+      <div className="title-container">
+        <div className="back-button">
+          <Link to="/">
+            <BackIcon style={{ width: "100px", height: "100px" }} />
+          </Link>
+        </div>
+        <h1>{episode.title}</h1>
+      </div>
+      <p className="details">
+        <span className="date">{episode.date}</span> ·{" "}
         <span className="duration">{episode.duration}</span>
       </p>
-      { episode.cover ? <img className="episode-cover" src={episode.cover} alt="coverart" /> : <></> }
-      <p className="summary">{unshortifyEmoji(episode.summary)}</p>
-      <AudioPlayer
-        src={episode.audio}
-        customAdditionalControls={[]}
+      <img
+        className="episode-cover"
+        src={episode.cover ? episode.cover : "/logo512.png"}
+        alt="coverart"
       />
-      <p className="description" dangerouslySetInnerHTML={{ __html: unshortifyEmoji(episode.content)}} />
+      <AudioPlayer src={episode.audio} customAdditionalControls={[]} />
+      {episode.summary ? (
+        <p className="summary">{unshortifyEmoji(episode.summary)}</p>
+      ) : (
+        <></>
+      )}
+      <h2>Shownotes</h2>
+      <p
+        className="description"
+        dangerouslySetInnerHTML={{ __html: unshortifyEmoji(episode.content) }}
+      />
       <Footer />
     </>
   );
