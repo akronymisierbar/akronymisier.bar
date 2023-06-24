0709ac98b0b266e1bec49fc7151a481eee8e4b83
Kilian Koeltzsch <me@kilian.io>
2023-06-25 00:29:17 +0200

    move links to top of page and delete unused css

--- src/App.tsx
+++ src/App.tsx
@@ -19,38 +19,40 @@ function App() {
   return (
     <div className="App">
       <header>
+        <ul className="links">
+          <li key="rss">
+            <a href="https://feed.akronymisier.bar">RSS Feed</a>
+          </li>
+          <li key="itunes">
+            <a href="https://itunes.apple.com/de/podcast/akronymisierbar/id1200334668">
+              Apple Podcasts
+            </a>
+          </li>
+          <li key="matrix">
+            <a href="https://matrix.to/#/#akronymisierbar:matrix.org">Matrix</a>
+          </li>
+          <li key="fediverse">
+            <a href="https://chaos.social/@akronymisierbar">Fediverse</a>
+          </li>
+          <li key="liberapay">
+            <a href="https://liberapay.com/akronymisierbar/">Liberapay</a>
+          </li>
+        </ul>
+
         <img
           className="logo center"
           src="logo512.png"
           alt="podcast logo showing headphones and the text Akronymisierbar"
         />
       </header>
+
       <p>
         Podcast von und mit <a href="https://chaos.social/@hoodie">@hoodie</a>{" "}
         und <a href="https://chaos.social/@kilian">@kilian</a>. Geballtes
         gefährliches Halbwissen zu allem rund um Programmiersprachen, Messengern
         und anderen (meist technischen) Themen, die uns spontan einfallen.
       </p>
-      <h2>Links</h2>
-      <ul>
-        <li key="rss">
-          <a href="https://feed.akronymisier.bar">RSS Feed</a>
-        </li>
-        <li key="itunes">
-          <a href="https://itunes.apple.com/de/podcast/akronymisierbar/id1200334668">
-            Apple Podcasts
-          </a>
-        </li>
-        <li key="matrix">
-          <a href="https://matrix.to/#/#akronymisierbar:matrix.org">Matrix</a>
-        </li>
-        <li key="fediverse">
-          <a href="https://chaos.social/@akronymisierbar">Fediverse</a>
-        </li>
-        <li key="liberapay">
-          <a href="https://liberapay.com/akronymisierbar/">Liberapay</a>
-        </li>
-      </ul>
+
       <h2>Folgen</h2>
       {episodes.map((ep, index) => (
         <>
