6ce3694f216213f5673597b0b84a764450dc621e
Kilian Koeltzsch <me@kilian.io>
2023-08-04 23:46:45 +0200

    add shebang and usage to chapter conversion script

--- scripts/convert-chaptermarks.ts
+++ scripts/convert-chaptermarks.ts
@@ -1,3 +1,5 @@
+#! /usr/bin/env deno run --allow-read --allow-write
+
 const convertToSeconds = (time: string) => {
   const [hours, minutes, seconds] = time.split(':').map(parseFloat);
   return hours * 3600 + minutes * 60 + seconds;
@@ -23,5 +25,10 @@ const generateJson = async (inputFile: string, outputFile: string) => {
 
   await Deno.writeTextFile(outputFile, JSON.stringify(result, null, 2));
 };
+
+if (Deno.args.length !== 2) {
+  console.log("Usage: ./convert-chaptermarks.ts <episode>.chapters.{txt,json}");
+  Deno.exit(1);
+}
 
 generateJson(Deno.args[0], Deno.args[1]);
