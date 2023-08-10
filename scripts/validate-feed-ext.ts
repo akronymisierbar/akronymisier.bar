c7801660bced19b65e560341a9bfca5b2a694245
Kilian Koeltzsch <me@kilian.io>
2023-08-10 18:57:54 +0200

    fix parse error and fetch feed through arg

--- scripts/validate-feed-ext.ts
+++ scripts/validate-feed-ext.ts
@@ -2,9 +2,12 @@
 
 import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.38/deno-dom-wasm.ts";
 
-const res = await fetch(
-  "https://validator.w3.org/feed/check.cgi?url=https://akronymisier.bar/feed.rss"
-);
+if (Deno.args.length !== 1) {
+  console.log("Usage: ./validate-feed-ext.ts <feed-url>");
+  Deno.exit(1);
+}
+
+const res = await fetch(Deno.args[0]);
 const html = await res.text();
 
 const doc = new DOMParser().parseFromString(html, "text/html")!;
@@ -13,16 +16,21 @@ const columnRegex = /column (\d+):/;
 
 // deno-lint-ignore no-explicit-any
 function printIssue(issue: any, severity: string) {
-  const line = issue
-    .querySelector("a:nth-child(1)")
-    .textContent.replace("line ", "");
+  let line = "0";
+  let column = "0";
+  if (issue.querySelector("a:nth-child(1)")) {
+    line = issue
+      .querySelector("a:nth-child(1)")
+      .textContent.replace("line ", "");
+    const p = issue.querySelector("p:nth-child(1)").textContent;
+    column = p.match(columnRegex)[1];
+  }
   const message = issue.querySelector("span.message").textContent;
+
   // const feedItem = issue
   //   .querySelector("blockquote > pre")
   //   .textContent.replace("^", "")
   //   .trim();
-  const p = issue.querySelector("p:nth-child(1)").textContent;
-  const column = p.match(columnRegex)[1];
 
   console.log(`::${severity} file=public/feed.rss,line=${line},col=${column}::${message}`);
 }
@@ -31,16 +39,12 @@ const issues = doc
   .querySelector("#main > ul:nth-child(4)")!
   .querySelectorAll("li");
 if (issues.length !== 0) {
-  // console.log(`Found ${issues.length} feed validation issue(s):`);
   issues.forEach((i) => printIssue(i, "error"));
-  // console.log("");
 }
 
 const compats = doc
   .querySelector("#main > ul:nth-child(6)")!
   .querySelectorAll("li");
 if (compats.length !== 0) {
-  // console.log(`Found ${compats.length} compatibility issue(s):`);
   compats.forEach((i) => printIssue(i, "warning"));
-  // console.log("");
 }
